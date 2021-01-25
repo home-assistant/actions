5aeee580948f8f1891236ed2da70bae743f48438
Ludeeus <ludeeus@ludeeus.dev>
2021-01-25 16:23:40 +0000

    test

--- hassfest/entrypoint.sh
+++ hassfest/entrypoint.sh
@@ -12,7 +12,4 @@ if [[ ${#integrations[@]} -eq 0 ]]; then
     bashio::exit.nok "No integrations found!"
 fi
 
-echo "* [WARNING] [TRANSLATIONS] Invalid switch.en.json: Device class need to start with 'test__'. Key string1 is invalid. See https://developers.home-assistant.io/docs/internationalization/core#stringssensorjson for dictionary value @ data['state']. Got {'string1': 'Value 1', 'string2': 'Value 2'}"
-echo "* [ERROR] [MANIFEST] Invalid manifest: Documentation url is not prefixed with https for dictionary value @ data['documentation']. Got 'http://example.com'"
-
 exec python3 -m script.hassfest --action validate "${integrations[@]}"
