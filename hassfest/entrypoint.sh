3516188c0268acb4d0b0a7a984f9668c348deca0
Joakim Sørensen <ludeeus@ludeeus.dev>
2023-06-29 08:59:54 +0200

    Workaround issue with hassfest not in site-packages (#90)

--- hassfest/entrypoint.sh
+++ hassfest/entrypoint.sh
@@ -4,12 +4,13 @@ declare integration_path
 
 shopt -s globstar nullglob
 for manifest in **/manifest.json; do
-    integration_path=$(dirname "${manifest}")
-    integrations+=(--integration-path "${integration_path}")
+    manifest_path=$(realpath "${manifest}")
+    integrations+=(--integration-path "${manifest_path%/*}")
 done
 
 if [[ ${#integrations[@]} -eq 0 ]]; then
     bashio::exit.nok "No integrations found!"
 fi
 
+cd /usr/src/homeassistant
 exec python3 -m script.hassfest --action validate "${integrations[@]}"
